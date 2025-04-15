76c7c7243b45b4c67507b5ffda56e0d81feba9a3
rjsgud49 <qkrrjsgud49@gmail.com>
2025-04-15 11:58:02 +0900

    로그인 파트에 Redux도입
    app.jsx나 모든 페이지에서 헤더로 state보낼필요 없음

--- ROOMIT/src/features/auth/authSlice.jsx
+++ ROOMIT/src/features/auth/authSlice.jsx
@@ -15,12 +15,12 @@ const authSlice = createSlice({
         login: (state, action) => {
             state.isLoggedIn = true;
             state.currentUser = action.payload;
-            localStorage.setItem('currentUser', JSON.stringify(action.payload)); // 현재 로그인 유저 저장
+            localStorage.setItem('currentUser', JSON.stringify(action.payload));
         },
         logout: (state) => {
             state.isLoggedIn = false;
             state.currentUser = null;
-            localStorage.removeItem('currentUser'); // 로그인 상태 제거
+            localStorage.removeItem('currentUser');
         },
     },
 });
