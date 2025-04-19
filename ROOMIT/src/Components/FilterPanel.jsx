8dfc5d6845e1089914ecc3847405bf90cba6af89
Qlellow <shinhyeok7306@gmail.com>
2025-04-20 02:58:14 +0900

    필터 위치 원위치

--- ROOMIT/src/Components/FilterPanel.jsx
+++ ROOMIT/src/Components/FilterPanel.jsx
@@ -1,4 +1,5 @@
 import "./css/FilterPanel.css";
+import { Funnel } from "lucide-react";
 
 // const filterOptions = {
 //   임대유형: ["임대 유형"],
@@ -7,23 +8,31 @@ import "./css/FilterPanel.css";
 //   presentPersons: [""],
 // };
 
-const FilterPanel = () => {
+const FilterPanel = ({ open, setOpen }) => {
+  const togglePanel = () => {
+    setOpen(!open);
+    console.log(open);
+  };
+
   return (
     <div className="filterPanel">
-      <div className="filterHeader">
-        <strong>🔍 필터 설정</strong>
-      </div>
+      <div className={`filterPanel ${open ? "open" : ""}`}>
+        <div className="filterHeader">
+          <strong>🔍 필터 설정</strong>
+          <button onClick={togglePanel}>✕</button>
+        </div>
 
-      <div className="filterOptions">
-        <div>▸ 카테고리</div>
-        <div>▸ 가격대</div>
-        <div>▸ 브랜드</div>
-        <div>▸ 평점</div>
-      </div>
+        <div className="filterOptions">
+          <div>▸ 카테고리</div>
+          <div>▸ 가격대</div>
+          <div>▸ 브랜드</div>
+          <div>▸ 평점</div>
+        </div>
 
-      <div className="filterFooter">
-        <button className="btn btn-reset">초기화</button>
-        <button className="btn btn-apply">적용</button>
+        <div className="filterFooter">
+          <button className="btn btn-reset">초기화</button>
+          <button className="btn btn-apply">적용</button>
+        </div>
       </div>
     </div>
   );
--- ROOMIT/src/Pages/LivingSpace.jsx
+++ ROOMIT/src/Pages/LivingSpace.jsx
@@ -18,7 +18,13 @@ const LivingSpace = ({ LivingSpaceData }) => {
     <div className="livingSpace">
       <Header />
       <div className="container">
-        <div className="filter">
+        <div className="title">
+          <h1>주거공간</h1>
+          <h2>현재 올라온 매물을 확인해보세요!</h2>
+          <button className="btn-filter" onClick={togglePanel}>
+            <Funnel size={17} />
+            필터
+          </button>
           <FilterPanel
             togglePanel={togglePanel}
             open={open}
@@ -26,18 +32,12 @@ const LivingSpace = ({ LivingSpaceData }) => {
             LivingSpaceData={LivingSpaceData}
           />
         </div>
-        <div className="livingSpace-content">
-          <div className="title">
-            <h1>주거공간</h1>
-            <h2>현재 올라온 매물을 확인해보세요!</h2>
-          </div>
-          <div className="livingSpace-list">
-            {filteredLivingSpaces
-              .filter((LivingSpaceData) => LivingSpaceData && LivingSpaceData.id) // 존재하고 id가 있는 유저만 통과
-              .map((LivingSpaceData, index) => (
-                <LivingSpaceItem key={index} LivingSpaceData={LivingSpaceData} />
-              ))}
-          </div>
+        <div className="livingSpace-list">
+          {filteredLivingSpaces
+            .filter((LivingSpaceData) => LivingSpaceData && LivingSpaceData.id) // 존재하고 id가 있는 유저만 통과
+            .map((LivingSpaceData, index) => (
+              <LivingSpaceItem key={index} LivingSpaceData={LivingSpaceData} />
+            ))}
         </div>
       </div>
     </div>
